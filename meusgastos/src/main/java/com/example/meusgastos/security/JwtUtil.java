fb15d5fd3cdcfc084e84aa72a86496b93031c241
Tamilyn <128110443+babimeaw@users.noreply.github.com>
2023-08-09 10:35:22 -0300

    Update JwtUtil.java

--- meusgastos/src/main/java/com/example/meusgastos/security/JwtUtil.java
+++ meusgastos/src/main/java/com/example/meusgastos/security/JwtUtil.java
@@ -2,13 +2,10 @@ package com.example.meusgastos.security;
 
 import java.security.Key;
 import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
 import com.example.meusgastos.domain.model.Usuario;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
