de8d4b37276e66a63502e5e1d4cae9146a8321c5
Tamilyn <128110443+babimeaw@users.noreply.github.com>
2023-08-09 10:34:34 -0300

    Update RestExceptionHandler.java

--- meusgastos/src/main/java/com/example/meusgastos/handler/RestExceptionHandler.java
+++ meusgastos/src/main/java/com/example/meusgastos/handler/RestExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.example.meusgastos.handler;
 
 import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import com.example.meusgastos.common.ConversorData;
 import com.example.meusgastos.domain.exception.ResourceBadRequestException;
 import com.example.meusgastos.domain.exception.ResourceNotFoundException;
